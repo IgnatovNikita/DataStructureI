f1b389ccd2e0c7a18bc17dcb3897631cd305fc76
IgnatovNikita <star08409@gmail.com>
2022-07-30 14:38:23 +0300

    Решена на объеденение двух массивов (тупое решение,но мне лень)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,10 @@ public class Main {
 	// write your code here
         //ContainsDuplicate cd = new ContainsDuplicate();
         //  MaximumSubarray ms = new MaximumSubarray();
-        int[] nums  = {3,2,4,5};
+        //int[] nums  = {3,2,4,5};
       //  System.out.println(cd.containsDuplicate(nums));
         //System.out.println(ms.maxSubArray(nums));
-        System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 6)));
+        // System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 6)));
+        new MergeSortedArray().merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
     }
 }
