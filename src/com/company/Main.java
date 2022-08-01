1345e75caa3fbbb900f7d543f091defb36382fc1
IgnatovNikita <star08409@gmail.com>
2022-08-01 20:11:48 +0300

    Решена задача на поиск максимальной разницы цены в массиве

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,13 @@ public class Main {
 	// write your code here
         //ContainsDuplicate cd = new ContainsDuplicate();
         //  MaximumSubarray ms = new MaximumSubarray();
-        int[] nums1  = {4,9,5};
-        int[] nums2 = {9,4,9,8,4};
+        int[] nums1  = {1,2};
+        int[] nums2 = {7,1,5,3,6,4};
       //  System.out.println(cd.containsDuplicate(nums));
         //System.out.println(ms.maxSubArray(nums));
         // System.out.println(Arrays.toString(new TwoSum().twoSum(nums, 6)));
         // new MergeSortedArray().merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
-        System.out.println(Arrays.toString(new IntersectionofTwoArraysII().intersect(nums1, nums2)));
+       // System.out.println(Arrays.toString(new IntersectionofTwoArraysII().intersect(nums1, nums2)));
+        System.out.println(new BestTimetoBuyandSellStock().maxProfit(nums1));
     }
 }
